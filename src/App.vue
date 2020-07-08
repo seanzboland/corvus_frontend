2b3ecd501c5518b58ca509828d8bed412b778ad4
seanzboland <sean.z.boland@gmail.com>
2020-07-08 17:24:00 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
+body,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.h-100 {
+  height: 100%;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.flex-row {
+  flex-direction: row;
+}
+
+.align-items-center {
+  align-items: center;
+}
+
+.justify-content-center {
+  justify-content: center;
+}
+
+.justify-space-between {
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <nav id="navbar" class="navbar d-flex align-items-center flex-row">
+    <!-- <div class="nav-brand">
+      <router-link to="/">
+        <img src="/img/logo.png" class="logo" alt="corvus logo" />
+      </router-link>
+    </div> -->
+    <div class="navbar-collapse h-100 d-flex align-items-center flex-row justify-space-between">
+      <ul class="navbar-nav d-flex align-items-center flex-row h-100">
+        <li class="nav-item h-100" v-for="(item, index) in navLinks" :key="index">
+          <router-link :to="item.to" class="nav-link h-100 d-flex align-items-center" >{{item.text}}</router-link>
+        </li>
+      </ul>
+      <button class="button-sign-out">Sign out</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      navLinks: [
+        { to: "/", text: "Warehouse" },
+        { to: "/Discrepancy", text: "Discrepancy ananlysis" },
+        { to: "/Reports", text: "Reports" },
+        { to: "/Flight", text: "Flight data" },
+      ]
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+  .navbar {
+    background-color: #fff;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+    padding: 0 60px;
+    height: 75px;
+  }
+  .navbar-nav {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  .nav-link {
+    padding: 0 15px;
+    font-size: 18px;
+    color: #000;
+    font-weight: 500;
+    position: relative;
+  }
+  .nav-link::after {
+    content: '';
+    position: absolute;
+    transition: 0.3s ease;
+    bottom: 0;
+    left: 0;
+    height: 5px;
+    width: 100%;
+    background-color: #0000ff63;
+    opacity: 0;
+  }
+  .navbar-collapse {
+    flex: 1;
+  }
+  .nav-link.router-link-exact-active::after {
+    opacity: 1;
+    width: 100%;
+  }
+  .button-sign-out {
+    border: 3px solid #0000ff63;
+    background-color: transparent;
+    height: 50px;
+    width: 150px;
+    border-radius: 5px;
+    font-size: 18px;
+    font-weight: 500;
+  }
+</style>
