b16c18f93ea93a43825c1681c2599361e858fd52
seanzboland <sean.z.boland@gmail.com>
2020-07-21 12:43:38 +0500

    work update

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,17 @@
 
 <script>
 import Navbar from "@/components/Navbar";
-
+import axios from 'axios'
 export default {
   components: {
     Navbar
+  },
+  mounted() {
+    axios
+      .get('/aisles/a1')
+      .then(response => {
+        console.log('response', response)
+      })
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import { BootstrapVue } from 'bootstrap-vue'
 import './assets/scss/app.scss'
 
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://ec2-3-101-47-119.us-west-1.compute.amazonaws.com:8080/api'
+axios.defaults.withCredentials = false
+axios.defaults.headers['Accept'] = 'application/json'
+axios.defaults.headers['Content-Type'] = 'application/json'
 
 new Vue({
   router,
